5d771ce03770bf697ff77b8114bb96da7ace3f4e
Michael Kozii <michael.koziy@gmail.com>
2025-07-28 17:38:13 +0200

    Add development dockerˆ

---
+++ docker/postgres/init.sql
@@ -0,0 +1,9 @@
+-- Optional PostgreSQL initialization scripts
+-- This file will be executed when the PostgreSQL container starts for the first time
+
+-- Create any additional databases, users, or configurations here if needed
+-- The main database 'laravel_dating' is already created via environment variables
+
+-- Example: Create additional test database (uncomment if needed)
+-- CREATE DATABASE laravel_dating_test;
+-- GRANT ALL PRIVILEGES ON DATABASE laravel_dating_test TO laravel_user;
